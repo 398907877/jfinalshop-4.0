224889126ccc69380bbb8814fb911bf64f9d014e
江南红衣 <hycx227@163.com>
2017-01-23 11:00:29 +0800

    技术交流群2：304061112，群1已满。

--- jfinalshop-4.0-core/src/main/java/com/jfinalshop/App.java
+++ jfinalshop-4.0-core/src/main/java/com/jfinalshop/App.java
@@ -4,10 +4,8 @@ package com.jfinalshop;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
