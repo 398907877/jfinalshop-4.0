b0292061bcc0da980b420d66d2bedeeab02f0cad
江南红衣 <hycx227@163.com>
2017-01-23 11:10:07 +0800

    技术交流群2：304061112，群1已满

--- jfinalshop-4.0-dao/src/main/java/com/jfinalshop/App.java
+++ jfinalshop-4.0-dao/src/main/java/com/jfinalshop/App.java
@@ -1,8 +1,13 @@
 package com.jfinalshop;
 
 /**
- * Hello world!
- *
+ * 捐赠后获取源码
+ * 商用下载jfinalshop 4.0二次开发，详细介绍请先联系QQ:187048359
+ * 技术交流群2：304061112，群1已满。
+ * 查看演示环境
+ * http://demo.jfinalshop.com
+ * http://demo.jfinalshop.com/admin
+ * jack 123456
  */
 public class App {
 	public static void main(String[] args) {
--- jfinalshop-4.0-web/src/main/java/com/jfinalshop/controller/TestController.java
+++ jfinalshop-4.0-web/src/main/java/com/jfinalshop/controller/TestController.java
@@ -2,6 +2,15 @@ package com.jfinalshop.controller;
 
 import com.jfinal.core.Controller;
 
+/**
+ * 捐赠后获取源码
+ * 商用下载jfinalshop 4.0二次开发，详细介绍请先联系QQ:187048359
+ * 技术交流群2：304061112，群1已满。
+ * 查看演示环境
+ * http://demo.jfinalshop.com
+ * http://demo.jfinalshop.com/admin
+ * jack 123456
+ */
 public class TestController extends Controller {
 
 	public void index() {
